aecf73ce23354ccc1984f64bcf52c0265d2caa99
Julian Gehring <julian.gehring@gmail.com>
2014-10-29 15:15:24 +0100

    show method for BootstrapCI

--- src/show.jl
+++ src/show.jl
@@ -1,9 +1,18 @@
 function Base.show(io::IO, x::BootstrapSample)
     r = 12
-    println("BootstrapSample")
+    println("Bootstrap Sampling")
     println(rpad("  Method:", r), x.method)
     println(rpad("  Samples:", r), x.m)
     println(rpad("  t0:", r), x.t0)
     println(rpad("  t1:", r), x.t1)
     println(rpad("  Data:", r), x.x)
+end
+
+function Base.show(io::IO, x::BootstrapCI)
+    r = 12
+    println("Bootstrap Confidence Interval")
+    println(rpad("  Method:", r), x.method)
+    println(rpad("  Level:", r), x.level)
+    println(rpad("  t0:", r), x.t0)
+    println(rpad("  Interval:", r), interval(x))
 end
