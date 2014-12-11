c45d3d02dd94429cef3d941315e129fb7ca744a2
Julian Gehring <julian.gehring@gmail.com>
2014-12-11 20:22:00 +0100

    tests: Extra tests for boot_exact

--- test/boot.jl
+++ test/boot.jl
@@ -46,4 +46,49 @@ for r in data, f in funs, b in b_methods
     end
 end
 
+## small version for boot_exact
+nSamples = 0;
+
+## sample data
+ru = rand(8);
+data = (ru, );
+
+## statistic functions
+funs = (mean, median)
+b_methods = (:exact, )
+ci_methods = (:basic, :perc, :normal)
+
+for r in data, f in funs, b in b_methods
+    bs = boot(r, f, nSamples, method = b)
+    ## testing
+    ## return type
+    @test isa(bs, BootstrapSample)
+    ## estimate t0
+    @test length(estimate(bs)) == 1
+    @test estimate(bs) == f(r)
+    ## straps t1
+    @test length(straps(bs)) > 1
+    ## accessors
+    @test method(bs) == b
+    ## bias and se
+    @test bias(bs) > -1.0 && bias(bs) < 1.0
+    @test se(bs) > 0
+    ## confidence intervals
+    for c in ci_methods
+        bci = ci(bs, method = c)
+        ## testing
+        ## return type
+        @test isa(bci, BootstrapCI)
+        ## estimate
+        @test estimate(bci) == estimate(bs)
+        ## accessors
+        @test method(bci) == c
+        ## default level
+        @test level(bci) == 0.95
+        ## CI bounds: interval
+        @test interval(bci)[1] < estimate(bci)
+        @test interval(bci)[2] > estimate(bci)
+    end
+end
+
 workspace() ## cleanup
