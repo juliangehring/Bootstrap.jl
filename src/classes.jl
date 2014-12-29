786d8fc5fecab01869ccb91c4b1932c93be15f02
Julian Gehring <julian.gehring@gmail.com>
2014-12-29 15:59:12 +0100

    Parametrize 'BootstrapSample' type, add doc

--- src/classes.jl
+++ src/classes.jl
@@ -1,12 +1,41 @@
-type BootstrapSample
-    t0
-    t1
+@doc """
+# Type 'BootstrapSample'
+
+## Definition
+
+`BootstrapSample{E,S,D}`
+
+## Fields
+
+- `t0 {E}`: Raw estimate, as given by `fun(x)`
+
+- `t1 {S}`: Bootstrapped estimates, also referred to as 'straps' here. A high-order o
+bject of type `S` with `m` elements, e.g. an `Array{Float64,1}` if `fun(x)` returned a `Float64`.
+
+- `x {D}`: Input data of type `D`. Currently, the types `AbstractVector`, `AbstractArray`, `DataFrame` are supported.
+
+- `m {Integer}`: The number of bootstrap samples that are computed
+
+- `fun {Function}`: Function to obtain the estimate given the data `x`. Note that at the moment, only `x` is passed to the function; additional arguments must be provided within the function or given as defaults. That means that function definition should look something like `fun(x) = ...`
+
+- `method {Symbol}`: The method/approach that is used for sampling the bootstrap estimates.
+
+## Notes
+
+An object of this type is returned by any of the 'boot*' methods. There is no good reason to construct one manually.
+
+""" ->
+type BootstrapSample{E,S,D}
+    t0::E
+    t1::S
     fun::Function
-    x::AbstractVector
+    x::D
     m::Integer
     wv
     method::Symbol
 end
+
+#bs = BootstrapSample(1.0, [1.0, 1.1, 1.2], mean, [1, 2, 3], 3, :basic)
 
 function bias(bs::BootstrapSample)
     b = mean(bs.t1) - bs.t0
