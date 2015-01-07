8f091848f968976fb9099236678bd055d22c21dd
Julian Gehring <julian.gehring@gmail.com>
2015-01-07 19:36:16 +0100

    test: sample of arrays

--- src/sample.jl
+++ src/sample.jl
@@ -33,7 +33,8 @@ end
 ### Sampling methods for 'Array'
 
 function sample{T}(a::AbstractArray{T}, d::Integer, n::Integer; replace::Bool=true, ordered::Bool=false)
-    index = sample([1:size(a, d)], n, replace = replace, ordered = ordered)
+    index = sample(1:size(a, d), n, replace = replace, ordered = ordered)
     a = slicedim(a, d, index)
     return a
 end
+
--- test/sampling.jl
+++ test/sampling.jl
@@ -18,3 +18,10 @@ dfu = sample(df, m, replace = false)
 
 dfw = sample(df, wv, m)
 @test nrow(dfw) == m
+
+### Sampling of Arrays ###
+a = rand(5, 3)
+as = sample(a, 1, size(a, 1))
+@test size(as) == size(a)
+as = sample(a, 2, size(a, 2))
+@test size(as) == size(a)
