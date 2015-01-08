d2b6aee6d0e2cdcf35968226dc1dff0153671b72
Julian Gehring <julian.gehring@gmail.com>
2015-01-08 01:19:42 +0100

    boot methods for DataFrame input

--- src/boot.jl
+++ src/boot.jl
@@ -14,6 +14,7 @@ function boot(x::AbstractVector, fun::Function, m::Int; method::Symbol = :basic)
 
 end
 
+### boot_basic ###
 
 function boot_basic(x::AbstractVector, fun::Function, m::Int)
     n = length(x)
@@ -28,6 +29,21 @@ function boot_basic(x::AbstractVector, fun::Function, m::Int)
     return res
 end
 
+function boot_basic(x::DataFrames.DataFrame, fun::Function, m::Int)
+    n = nrow(x)
+    t0 = fun(x)
+    t1 = zeros(typeof(t0), m)
+    for i in 1:m
+        t1[i] = fun(sample(x, n, replace = true))
+    end
+    res = BootstrapSample(t0, t1, fun, x, m, 0, :basic)
+
+    return res
+end
+
+
+### boot_weight ###
+
 function boot_weight(x::AbstractVector, fun::Function, m::Int, weight::WeightVec)
     n = length(x)
     t0 = fun(x)
@@ -41,6 +57,21 @@ function boot_weight(x::AbstractVector, fun::Function, m::Int, weight::WeightVec
     return res
 end
 
+
+function boot_weight(x::DataFrame, fun::Function, m::Int, weight::WeightVec)
+    n = nrow(x)
+    t0 = fun(x)
+    t1 = zeros(typeof(t0), m)
+    for i in 1:m
+        t1[i] = fun(sample(x, weight, n, replace = true))
+    end
+    res = BootstrapSample(t0, t1, fun, x, m, weight, :weighted)
+
+    return res
+end
+
+
+### boot_balanced ###
 
 function boot_balanced(x::AbstractVector, fun::Function, m::Int)
     n = length(x)
@@ -57,6 +88,23 @@ function boot_balanced(x::AbstractVector, fun::Function, m::Int)
     return res
 end
 
+function boot_balanced(x::DataFrames.DataFrame, fun::Function, m::Int)
+    n = nrow(x)
+    t0 = fun(x)
+    t1 = zeros(typeof(t0), m)
+    idx = repmat([1:n], m)
+    ridx = zeros(Integer, n, m)
+    sample!(idx, ridx, replace = false)
+    for i in 1:m
+        t1[i]= fun(x[ridx[:,i]],:)
+    end
+    res = BootstrapSample(t0, t1, fun, x, m, 0, :balanced)
+
+    return res
+end
+
+
+### boot_exact ###
 
 function boot_exact(x::AbstractVector, fun::Function)
     n = length(x)
@@ -70,3 +118,17 @@ function boot_exact(x::AbstractVector, fun::Function)
 
     return res
 end
+
+
+function boot_exact(x::DataFrames.DataFrame, fun::Function)
+     n = nrow(x)
+     t0 = fun(x)
+     m = binomial(2*n-1, n)
+     t1 = zeros(typeof(t0), m)
+     for (i, s) in enumerate(sample_exact(n))
+         t1[i] = fun(x[s,:])
+     end
+     res = BootstrapSample(t0, t1, fun, x, m, 0, :exact)
+
+     return res
+ end
