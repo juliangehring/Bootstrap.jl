645e8d68f1a6bea24903f19d80e96e260737d78d
David Widmann <dev+git@devmotion.de>
2019-08-30 17:07:30 +0200

    Simplify filenames

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using SafeTestsets
 
-@safetestset "Non-parametric bootstrap" begin include("test-bootsample-non-parametric.jl") end
-@safetestset "Parametric bootstrap" begin include("test-bootsample-parametric.jl") end
-@safetestset "Utilities" begin include("test-utils.jl") end
-@safetestset "Dependencies" begin include("test-dependencies.jl") end
+@safetestset "Non-parametric bootstrap" begin include("nonparametric.jl") end
+@safetestset "Parametric bootstrap" begin include("parametric.jl") end
+@safetestset "Utilities" begin include("utils.jl") end
+@safetestset "Dependencies" begin include("dependencies.jl") end
